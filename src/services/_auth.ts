e97556722c9801092e74d5ff74f46bc8da044050
arvinxx <arvinx@foxmail.com>
2025-01-01 22:52:08 +0800

    fix auth

--- src/services/_auth.ts
+++ src/services/_auth.ts
@@ -1,16 +1,29 @@
 import { JWTPayload, LOBE_CHAT_AUTH_HEADER } from '@/const/auth';
+import { isDeprecatedEdition } from '@/const/version';
 import { ModelProvider } from '@/libs/agent-runtime';
 import { aiProviderSelectors, useAiInfraStore } from '@/store/aiInfra';
 import { useUserStore } from '@/store/user';
 import { keyVaultsConfigSelectors, userProfileSelectors } from '@/store/user/selectors';
-import { GlobalLLMProviderKey } from '@/types/user/settings';
+import {
+  AWSBedrockKeyVault,
+  AzureOpenAIKeyVault,
+  CloudflareKeyVault,
+  OpenAICompatibleKeyVault,
+  WenxinKeyVault,
+} from '@/types/user/settings';
 import { createJWT } from '@/utils/jwt';
 
-export const getProviderAuthPayload = (provider: string) => {
+export const getProviderAuthPayload = (
+  provider: string,
+  keyVaults: OpenAICompatibleKeyVault &
+    AzureOpenAIKeyVault &
+    AWSBedrockKeyVault &
+    WenxinKeyVault &
+    CloudflareKeyVault,
+) => {
   switch (provider) {
     case ModelProvider.Bedrock: {
-      const { accessKeyId, region, secretAccessKey, sessionToken } =
-        keyVaultsConfigSelectors.bedrockConfig(useUserStore.getState());
+      const { accessKeyId, region, secretAccessKey, sessionToken } = keyVaults;
 
       const awsSecretAccessKey = secretAccessKey;
       const awsAccessKeyId = accessKeyId;
@@ -27,9 +40,7 @@ export const getProviderAuthPayload = (provider: string) => {
     }
 
     case ModelProvider.Wenxin: {
-      const { secretKey, accessKey } = keyVaultsConfigSelectors.wenxinConfig(
-        useUserStore.getState(),
-      );
+      const { secretKey, accessKey } = keyVaults;
 
       const apiKey = (accessKey || '') + (secretKey || '');
 
@@ -41,36 +52,26 @@ export const getProviderAuthPayload = (provider: string) => {
     }
 
     case ModelProvider.Azure: {
-      const azure = keyVaultsConfigSelectors.azureConfig(useUserStore.getState());
-
       return {
-        apiKey: azure.apiKey,
-        azureApiVersion: azure.apiVersion,
-        baseURL: azure.endpoint,
+        apiKey: keyVaults.apiKey,
+        azureApiVersion: keyVaults.apiVersion,
+        baseURL: keyVaults.endpoint,
       };
     }
 
     case ModelProvider.Ollama: {
-      const config = keyVaultsConfigSelectors.ollamaConfig(useUserStore.getState());
-
-      return { baseURL: config?.baseURL };
+      return { baseURL: keyVaults?.baseURL };
     }
 
     case ModelProvider.Cloudflare: {
-      const config = keyVaultsConfigSelectors.cloudflareConfig(useUserStore.getState());
-
       return {
-        apiKey: config?.apiKey,
-        cloudflareBaseURLOrAccountID: config?.baseURLOrAccountID,
+        apiKey: keyVaults?.apiKey,
+        cloudflareBaseURLOrAccountID: keyVaults?.baseURLOrAccountID,
       };
     }
 
     default: {
-      const config = keyVaultsConfigSelectors.getVaultByProvider(provider as GlobalLLMProviderKey)(
-        useUserStore.getState(),
-      );
-
-      return { apiKey: config?.apiKey, baseURL: config?.baseURL };
+      return { apiKey: keyVaults?.apiKey, baseURL: keyVaults?.baseURL };
     }
   }
 };
@@ -94,10 +95,18 @@ export const createHeaderWithAuth = async (params?: AuthParams): Promise<Headers
   let payload = params?.payload || {};
 
   if (params?.provider) {
-    const keyVaults = aiProviderSelectors.providerKeyVaults(params?.provider)(
-      useAiInfraStore.getState(),
-    );
-    payload = { ...payload, ...getProviderAuthPayload(params?.provider), ...keyVaults };
+    let keyVaults = {};
+
+    if (isDeprecatedEdition) {
+      keyVaults = keyVaultsConfigSelectors.getVaultByProvider(params.provider as any)(
+        useUserStore.getState(),
+      );
+    } else {
+      keyVaults =
+        aiProviderSelectors.providerKeyVaults(params?.provider)(useAiInfraStore.getState()) || {};
+    }
+
+    payload = { ...payload, ...getProviderAuthPayload(params?.provider, keyVaults) };
   }
 
   const token = await createAuthTokenWithPayload(payload);
